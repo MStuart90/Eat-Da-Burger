b0b2d8e28a135467b6d9c389cc701afca086e965
Melanie Stuart <mmooradian90@yahoo.com>
2020-12-03 18:19:59 -0500

    updating jawsdb login for deployment.

--- config/connection.js
+++ config/connection.js
@@ -4,20 +4,20 @@ var connection;
 
 // JawsDB is an add-on for providing a fully functional MySQL Database server for use with your Heroku application
 // The JAWSDB_URL config variable will contain a placeholder until the database is ready.
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWSDB_URL) {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
 	connection = mysql.createConnection({
-		host: "localhost",
+		host: "de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 		port: 3306,
-		user: "root",
-		password: "Root",
-		database: "burgers_db",
+		user: "jb7j0m0nu6m62hve",
+		password: "w76gk2e5s93ziz0l",
+		database: "g1fs4mjjs4wfaaom",
 	});
-}
+// }
 connection.connect(function (err) {
 	if (err) {
-		console.err("error connectiong: " + err.stack);
+		console.log("error connectiong: " + err.stack);
 		return;
 	}
 	console.log("connected as id " + connection.threadId);
